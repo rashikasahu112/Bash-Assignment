6856ca1d47c03bf55ccd19d34f79891327bc3447
rashikasahu112 <rashikasahu04@gmail.com>
2023-12-28 14:33:16 +0530

    changes in files

--- asg1.sh
+++ asg1.sh
@@ -1,38 +1,40 @@
 #!/bin/bash
 
+log_directory="files"
 
-log_file="files/apache-logs.log"
+# Ilerating over all log files in the directory
+for log_file in "$log_directory"/*.log; do
+  echo "Processing $log_file"
+  echo
 
-# Check if file exist or not
-if [ ! -e "$log_file" ]; then
-    echo "Log file does not exist"
-    exit 1
-fi
-
-# fetching the required information & displaying
-while IFS= read -r logs_entry; do
-    ip_address=$(echo "$logs_entry" | grep -oE '\b([0-9,x]{1,3}\.){3}[0-9,x]{1,3}\b') 
-    timestamp1=$(echo "$logs_entry" | grep -oE '\[[0-9]{2}/[A-Za-z]{3}/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} -[0-9]{4}\]')
-    timestamp2=$(echo "$logs_entry" | grep -oE '\[[A-Za-z]{3} [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}\]')
-    response_code=$(echo "$logs_entry" | grep -oE '(401|200) | head -n1')
-    url=$(echo "$logs_entry" | grep -oE '"(GET|PROPFIND) [^"]+ HTTP/1\.[01]"')
-
-    if [ -n "$ip_address" ]; then
-        echo "IP Address: $ip_address"
-    fi
-    
-    if [ -n "$timestamp1" ]; then
-        echo "Timestamp: $timestamp1"
-    elif [ -n "$timestamp2" ]; then
-        echo "Timestamp: $timestamp2"
-    fi
-    
-    if [ -n "$url" ]; then
-        echo "Url: $url"
-    fi
-    
-    if [ -n "$response_code" ]; then
-        echo "Response Code: $response_code"
-    fi
-    
-done < "$log_file"
+  # fetching the required information & displaying
+  while IFS= read -r logs_entry; do
+      ip_address=$(echo "$logs_entry" | grep -oE '\b([0-9,x]{1,3}\.){3}[0-9,x]{1,3}\b') 
+      timestamp1=$(echo "$logs_entry" | grep -oE '\[[0-9]{2}/[A-Za-z]{3}/[0-9]{4}:[0-9]{2}:[0-9]{2}:[0-9]{2} -[0-9]{4}\]')
+      timestamp2=$(echo "$logs_entry" | grep -oE '\[[A-Za-z]{3} [A-Za-z]{3} [0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{4}\]')
+      response_code=$(echo "$logs_entry" | grep -oE '(401|200) | head -n1')
+      url=$(echo "$logs_entry" | grep -oE '"(GET|PROPFIND) [^"]+ HTTP/1\.[01]"')
+  
+      if [ -n "$ip_address" ]; then
+          echo "IP Address: $ip_address"
+      fi
+      
+      if [ -n "$timestamp1" ]; then
+          echo "Timestamp: $timestamp1"
+      elif [ -n "$timestamp2" ]; then
+          echo "Timestamp: $timestamp2"
+      fi
+      
+      if [ -n "$url" ]; then
+          echo "Url: $url"
+      fi
+      
+      if [ -n "$response_code" ]; then
+          echo "Response Code: $response_code"
+      fi
+      
+  done < "$log_file"
+  
+  echo
+  echo
+done
--- asg4.sh
+++ asg4.sh
@@ -1,8 +1,36 @@
 #!/bin/bash
 
-#read -p "Enter the path to the log file: " log_file
 
-log_file="/home/rashikasahu/bash_asg/files/apache-logs.log"
+average_success_response_time(){
+  log_file=$1
+  total_response_time=0
+  success_response_count=0
+  
+  # fetching the response code & time
+  while IFS= read -r log_entry; do
+    success_response_code=$(echo "$log_entry" | grep -oE ' 2[0-9]{2} ' | tr -d ' ')
+    response_time=$(echo "$log_entry" | grep -oE 'HTTP/1\.[01]" 2[0-9]{2} ([0-9]+)' | awk '{print $3}')
+      
+    if [[ -n $success_response_code ]] && [[ -n $response_time ]]; then
+      success_response_count=$((success_response_count + 1))
+      total_response_time=$(awk "BEGIN {print $total_response_time + $response_time}")
+    fi
+  done < "$log_file"
+  
+  # Calculating average response time & displaying result
+  if [ "$success_response_count" -gt 0 ]; then
+      average_response_time=$(awk "BEGIN {print $total_response_time / $success_response_count}")
+      echo "Average response time for successful requests: $average_response_time milliseconds"
+  else
+      echo "No successfull requests found in the log file."
+  fi
+}
+
+
+# taking file path as user input
+read -p "Enter the path to the log file: " log_file
+
+# check if file exist or not
 if [ -z "$log_file" ]; then
     echo "Log file path cannot be empty"
     exit 1
@@ -11,22 +39,5 @@ elif [ ! -e "$log_file" ]; then
     exit 1
 fi
 
-total_response_time=0
-success_response_count=0
-
-while IFS= read -r log_entry; do  
-  success_response_code=$(echo "$log_entry" | grep -oE ' 2[0-9]{2} ' | tr -d ' ')
-  response_time=$(echo "$log_entry" | grep -oE 'HTTP/1\.[01]" 2[0-9]{2} ([0-9]+)' | awk '{print $3}')
-    
-  if [[ -n $success_response_code ]] && [[ -n $response_time ]]; then
-    success_response_count=$((success_response_count + 1))
-    total_response_time=$(awk "BEGIN {print $total_response_time + $response_time}")
-  fi
-done < "$log_file"
-
-if [ "$success_response_count" -gt 0 ]; then
-    average_response_time=$(awk "BEGIN {print $total_response_time / $success_response_count}")
-    echo "Average response time for successful requests: $average_response_time milliseconds"
-else
-    echo "No successful requests found in the log file."
-fi
+# determine average response time
+average_success_response_time $log_file
